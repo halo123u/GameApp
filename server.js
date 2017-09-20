3f7007ddd7ab473622b4b9a65e40e9dd9a1db24e
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-20 06:56:34 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
+require("dotenv").config();
+// const Promise = require('bluebird');
+// Promise.promisifyAll(mongoose);
+mongoose.connect('mongodb://localhost/GamingDb');
+
+app.use(bodyParse.urlencoded({ extended : true}));
+app.use(bodyParse.json());
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: l' + port);
