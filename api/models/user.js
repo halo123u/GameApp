9b475b056e4b28d073152be2ffaef6e8bb7ba732
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-20 07:22:25 -0400

    add authentication services

--- api/models/user.js
+++ api/models/user.js
@@ -87,7 +87,23 @@ UserSchema.statics = {
                 });
             });
         });
-    }
+    },
+    findByToken : function (token) {
+        const User = this;
+        var decoded;
+
+        try{
+            decoded = jwt.verify(token, 'abc123');
+        } catch (e){
+            return Promise.reject();
+        }
+
+        return User.findOne({
+            '_id': decoded._id,
+            'tokens.token': token,
+            'tokens.access' : 'auth'
+        });
+    },
 }
 
 
