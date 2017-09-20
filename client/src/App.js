61575ea724bbcde6ae8faa61e9442c28d6b31980
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-20 09:05:33 -0400

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,19 @@ import {
 } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Dashboard/>
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={()=><Login login={this.handleLoginSubmit} signup={this.handleSignUp}/>}/>
+            <Route exact path='/dashboard' component={Dashboard}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            email: '',
+            emails: '',
+            password: '',
+            passwords: ''
+        }
+    }
+    handleInputChange =  (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState ({
+          [name]: value
+        });
+      } 
+    render(){
+        return(
+        <div>
+            <h1>Log In</h1>
+            <form onSubmit={(e)=>this.props.login(e,this.state.email, this.state.password)}>
+                <input type="email" placeholder="Email" name="email" onChange={this.handleInputChange} value={this.state.email} /> <br/>
+                <input type="password" name="password" onChange={this.handleInputChange} value={this.state.password} placeholder="Password"/>
+                <button>login</button>
+            </form> <br/>
+            <h1>Sign Up</h1>
+            <form onSubmit={(e)=>this.props.signup(e, this.state.emails, this.state.gamertag, this.state.passwords)}>
+                <input type="email" placeholder="Email" name="emails" onChange={this.handleInputChange} value={this.state.emails} /> <br/>
+                <input type="password" name="passwords" onChange={this.handleInputChange} value={this.state.passwords} placeholder="Password"/>
+                <button>SignUp</button>
+            </form>
+
+        </div>
+
+        );
+    }
+}
+export default Login;
