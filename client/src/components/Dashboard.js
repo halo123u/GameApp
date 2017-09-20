701c8cf46ae126dfe3f0f0c9cd3b28a77b5b5eb3
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-20 09:01:54 -0400

    add dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';  
+
+class DashBoard extends Component{
+    render(){
+        return(
+            <h1>hello this is the DashBoard</h1>
+        )
+    }
+}
+
+export default DashBoard;
