bec69b12ab4e774b6290dee87d8d409dd5cc370d
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-21 20:36:36 -0400

    remove unneccessary constructor

--- api/routes/gameRoutes.js
+++ api/routes/gameRoutes.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const authenticate = require('../../services/auth/authenticate');
 const gameApi = express.Router();
 gameApi.get('/popular', authenticate , (req,res)=>{
-    // https://api-2445582011268.apicast.io/games/?fields=name,url,summary,popularity,cover&order=popularity:desc
     let header = { 'user-key' : process.env.USER_KEY} 
     axios.get(' https://api-2445582011268.apicast.io/games/?fields=name,url,summary,popularity,cover,screenshots&order=popularity:desc',{headers : header}).then(data=>{
         console.log(data);
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-    constructor(props){
-        super(props);
-    }
     render(){
         return(
         <nav className="navbar navbar-inverse navbar-fixed-top">
